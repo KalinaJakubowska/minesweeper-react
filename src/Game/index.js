071cb05c19c9a9e9c9ab8ad8ca3060c266b96b92
Kalina Jakubowska <witillie@gmail.com>
2020-07-27 16:24:44 +0200

    bombs generating added

--- src/Game/index.js
+++ src/Game/index.js
@@ -10,8 +10,25 @@ const Game = ({
     isGameLost,
     setIsGameLost,
     isGameWon,
-    setIsGameWon
+    setIsGameWon,
+    gameSize,
+    isItBeforeFirstLeftClick,
+    setIsItBeforeFirstLeftClick,
 }) => {
+
+    const idsAroundSelectedField = (index) => {
+        const idsAroundFieldTemplate = [
+            (gameLineColumns * (-1) - 1),
+            (gameLineColumns * (-1)),
+            (gameLineColumns * (-1) + 1),
+            -1,
+            1,
+            (gameLineColumns - 1),
+            (gameLineColumns),
+            (gameLineColumns + 1)
+        ];
+        return idsAroundFieldTemplate.map(id => id + index);
+    };
 
     const checkIsGameWon = () => {
         if (gameFields.filter(({ hidden }) => hidden).length === bombsNumber) {
@@ -26,10 +43,17 @@ const Game = ({
                 { ...gameFields[id], hidden: false },
                 ...gameFields.slice(id + 1),
             ])
+
         checkIsGameWon();
     };
 
     const checkField = (id) => {
+        if (isItBeforeFirstLeftClick) {
+            generateBombsPlaces(id);
+            setIsItBeforeFirstLeftClick(false);
+        }
+
+
         if (gameFields[id].type === "bomb") {
             setIsGameLost(true);
         } else if (gameFields[id].bombsAround === 0) {
@@ -38,6 +62,30 @@ const Game = ({
             revealField(id);
         }
     }
+
+    const generateBombsPlaces = (id) => {
+        const emptyFields = idsAroundSelectedField(id);
+        let newBombs = [];
+        let newBomb;
+        for (let i = 1; i <= bombsNumber; i++) {
+            newBomb = Math.floor(Math.random() * gameSize);
+            while (gameFields[newBomb].type !== "field"
+                || newBomb === id
+                || emptyFields.includes(newBomb)) {
+                newBomb = Math.floor(Math.random() * gameSize);
+            }
+            newBombs.push(newBomb);
+        }
+        newBombs.map(bomb => {
+            setGameFields(gameFields =>
+                [
+                    ...gameFields.slice(0, bomb),
+                    { ...gameFields[bomb], type: "bomb" },
+                    ...gameFields.slice(bomb + 1),
+                ]
+            );
+        })
+    };
 
     return (
         < div className="container container--game" >
@@ -52,12 +100,12 @@ const Game = ({
                     <div key={id} className={`game__field
                     ${type === "field" ? "" : " game__field--" + type}
                     ${isGameWon && type === "bomb" ? " game__field--greenBomb" : ""} 
-                    js-field`}>
+                    `}>
                         <button
                             className={
                                 `game__button
                                 ${!hidden ? " game__button--hidden" : ""}
-                                ${rightClicked ? " game__button--rightClicked" : ""} js-gameButton`}
+                                ${rightClicked ? " game__button--rightClicked" : ""}`}
                             onClick={() => checkField(id)}
                         >
                         </button>
