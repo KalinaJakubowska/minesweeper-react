a9a5b8d2a61d08a406c89bf4488ecdfa3973f898
Kalina Jakubowska <witillie@gmail.com>
2020-08-25 16:27:20 +0200

    GlobalStyle added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::before, ::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  min-height: 100vh;
+  position: relative;
+  font-size: 16px;
+  padding: 50px 0 70px;
+}
+`
