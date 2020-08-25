8451c8ee76cb2a2c73854fc57107005398d73f36
Kalina Jakubowska <witillie@gmail.com>
2020-08-25 16:56:17 +0200

    colors changed with theme

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -10,10 +10,10 @@ export const Wrapper = styled.form`
 export const Fieldset = styled.fieldset`
     display: flex;
     flex-direction: column;
-    background-color: white;
+    background-color: ${({ theme }) => theme.color.second};
     border: none;
-    border-top: 2px solid teal;
-    box-shadow: 0px 0px 10px 0px #ccc;
+    border-top: 2px solid ${({ theme }) => theme.color.first};
+    box-shadow: 0px 0px 10px 0px ${({ theme }) => theme.color.shadow};
     margin-top: 10px;
 
     ${({ hidden }) => hidden && css`
@@ -24,8 +24,8 @@ export const Legend = styled.legend`
     width: fit-content;
     text-align: center;
     background-color: rgba(0, 128, 128, 0.15);
-    border: 2px solid teal;
-    color: teal;
+    border: 2px solid ${({ theme }) => theme.color.first};
+    color: ${({ theme }) => theme.color.first};
     padding: 10px;
     font-weight: bold;
 `
@@ -38,15 +38,15 @@ export const Input = styled.input`
     margin-left: 20px;
 `
 export const Button = styled.button`
-    background-color: teal;
+    background-color: ${({ theme }) => theme.color.first};
     border: 2px solid hsl(180, 100%, 20%);
-    color: white;
+    color: ${({ theme }) => theme.color.second};
     padding: 10px;
     margin-top: 10px;
 
     &:hover {
     background-color: hsl(180, 100%, 30%);
-    border-color: teal;
+    border-color: ${({ theme }) => theme.color.first};
     }
 `
 export const ErrorInfoText = styled.span`
