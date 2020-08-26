221ca27cffe63fa2bdf9a1298eaa4481193b3616
Kalina Jakubowska <witillie@gmail.com>
2020-08-26 22:21:07 +0200

    Timer styled with styled-components

---
+++ src/Display/Timer/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.p`
+    font-size: 40px;
+    margin: 10px auto;
+    border-right: 3px solid teal;
+    border-left: 3px solid teal;
+    padding: 0px 20px;
+`
