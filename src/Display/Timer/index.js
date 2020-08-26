0bbb788ac7336caa2ab934910fb44f0808bbaeed
Kalina Jakubowska <witillie@gmail.com>
2020-08-26 22:20:49 +0200

    Timer added

---
+++ src/Display/Timer/index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Wrapper } from "./styled.js";
+
+const Timer = ({ isGameLost, isGameWon, timeData }) => {
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef(null);
+
+    useEffect(() => {
+        clearInterval(intervalRef.current);
+    }, [isGameLost, isGameWon])
+
+    useEffect(() => {
+        if (timeData) {
+            if (timeData.endDate) {
+                clearInterval(intervalRef.current);
+                setTime(timeData.endDate - timeData.startDate)
+            } else {
+                intervalRef.current = setInterval(() => {
+                    setTime(new Date() - timeData.startDate)
+                }, 10)
+            }
+        }
+
+        return () => clearInterval(intervalRef.current);
+    }, [timeData])
+
+    return (
+        <Wrapper>{(time / 1000).toFixed(2)}</Wrapper>
+    );
+}
+export default Timer;
