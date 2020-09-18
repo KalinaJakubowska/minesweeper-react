457f3ee8a29f56bdf2f1cdeff1ba90af2d064645
Kalina Jakubowska <witillie@gmail.com>
2020-09-18 11:57:23 +0200

    creating new field moved to a new file, setGameProperties created

--- src/features/gameSlice.js
+++ src/features/gameSlice.js
@@ -14,34 +14,21 @@ const gameSlice = createSlice({
         setGameFields: (state, { payload }) => {
             state.gameFields = payload;
         },
-        setGameLineColumns: (state, { payload }) => {
-            state.gameLineColumns = payload;
-        },
-        setGameLineRows: (state, { payload }) => {
-            state.gameLineRows = payload;
-        },
-        setBombsNumber: (state, { payload }) => {
-            state.bombsNumber = payload;
-        },
         setIsGameLost: (state, { payload }) => {
             state.isGameLost = payload;
         },
         setIsGameWon: (state, { payload }) => {
             state.isGameWon = payload;
         },
-        createNewField: (state, { payload }) => {
-            state.gameFields.push(
-                {
-                    id: state.gameFields.length,
-                    type: payload[0],
-                    hidden: payload[1],
-                    bombsAround: 0,
-                    rightClicked: false,
-                }
-            );
-        },
         revealField: ({ gameFields }, { payload: id }) => {
             gameFields[id].hidden = false;
+        },
+        setGameProperties: (state, { payload: { bombsNumber, gameLineColumns, gameLineRows } }) => {
+            state.gameLineColumns = gameLineColumns;
+            state.gameLineRows = gameLineRows;
+            state.bombsNumber = bombsNumber;
+            state.isGameLost = false;
+            state.isGameWon = false;
         },
     },
 });
@@ -53,8 +40,8 @@ export const {
     setGameLineColumns,
     setGameLineRows,
     setGameFields,
-    createNewField,
     revealField,
+    setGameProperties,
 } = gameSlice.actions;
 export const selectGameData = state => state.gameData;
 export const selectGameFields = state => state.gameData.gameFields;
