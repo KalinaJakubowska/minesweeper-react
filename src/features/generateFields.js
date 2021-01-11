64d81fb12eb5e2127a4d2d3eca8e306900f4e727
KalinaJakubowska <witillie@gmail.com>
2021-01-11 16:42:25 +0100

    use object instead of array when create a new field

--- src/features/generateFields.js
+++ src/features/generateFields.js
@@ -1,28 +1,31 @@
-let gameFields = []
+let gameFields = [];
 
-const createNewField = (payload) => {
-    gameFields.push(
-        {
-            id: gameFields.length,
-            type: payload[0],
-            hidden: payload[1],
-            bombsAround: 0,
-            rightClicked: false,
-        }
-    );
+const createNewField = ({ type, hidden }) => {
+  gameFields.push({
+    id: gameFields.length,
+    type,
+    hidden,
+    bombsAround: 0,
+    rightClicked: false,
+  });
 };
 
 export const generateFields = (gameLineColumns, gameLineRows) => {
-    gameFields = [];
-    for (let i = 0; i < gameLineRows; i++) {
-        for (let y = 0; y < gameLineColumns; y++) {
-            if (y === 0 || y === (gameLineColumns - 1) || i === 0 || i === (gameLineRows - 1)) {
-                createNewField(["border", false]);
-            } else {
-                createNewField(["field", true]);
-            }
-        }
-    };
+  gameFields = [];
+  for (let i = 0; i < gameLineRows; i++) {
+    for (let y = 0; y < gameLineColumns; y++) {
+      if (
+        y === 0 ||
+        y === gameLineColumns - 1 ||
+        i === 0 ||
+        i === gameLineRows - 1
+      ) {
+        createNewField({ type: "border", hidden: false });
+      } else {
+        createNewField({ type: "field", hidden: true });
+      }
+    }
+  }
 
-    return gameFields;
+  return gameFields;
 };
