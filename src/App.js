b344709ed150ebf560f07086d2f2be2f8dd9432c
KalinaJakubowska <witillie@gmail.com>
2021-01-26 22:11:11 +0100

    first fields generating moved to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import Form from "./features/Form";
 import Game from "./features/Game";
 import Footer from "./common/Footer";
@@ -6,8 +6,15 @@ import Display from "./features/Display";
 import { GlobalStyle } from "./GlobalStyle.js";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/theme/theme.js";
+import { useDispatch } from "react-redux";
+import { generateEmptyFields } from "./features/gameSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(generateEmptyFields());
+  }, [dispatch]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -18,6 +25,6 @@ function App() {
       <Footer />
     </ThemeProvider>
   );
-};
+}
 
 export default App;
