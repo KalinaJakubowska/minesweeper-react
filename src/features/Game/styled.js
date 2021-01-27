921a988c745efc9e8d4f0a2f840285d1072572a4
KalinaJakubowska <witillie@gmail.com>
2021-01-27 02:20:12 +0100

    opacity removed

--- src/features/Game/styled.js
+++ src/features/Game/styled.js
@@ -33,7 +33,6 @@ export const GameButton = styled.button`
     width: 40px;
     border-radius: 0;
     background-color: ${({ theme }) => theme.color.shadow};
-    opacity: 0.5;
     border: 2px outset ${({ theme }) => theme.color.second};
     ${({ hidden }) => hidden && css`
         display: none;
