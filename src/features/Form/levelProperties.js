792281ac70de17640acc051b6a713b57293e0342
KalinaJakubowska <witillie@gmail.com>
2021-02-02 17:34:03 +0100

    level properties moved to a new file

---
+++ src/features/Form/levelProperties.js
@@ -0,0 +1,17 @@
+export default {
+  easy: {
+    rows: 8,
+    columns: 8,
+    bombs: 10,
+  },
+  medium: {
+    rows: 16,
+    columns: 16,
+    bombs: 40,
+  },
+  expert: {
+    rows: 16,
+    columns: 30,
+    bombs: 99,
+  },
+};
