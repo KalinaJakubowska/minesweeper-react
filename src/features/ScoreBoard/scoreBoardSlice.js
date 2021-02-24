e5b2b2a8de4d79310c0b35cc94035b24f8c800f3
KalinaJakubowska <witillie@gmail.com>
2021-02-24 14:23:57 +0100

    add scoreBoardSlice

---
+++ src/features/ScoreBoard/scoreBoardSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const scoreBoardSlice = createSlice({
+  name: "scoreBoard",
+  initialState: {
+    bestResult: undefined,
+  },
+  reducers: {
+    updateBestResult: (state, { payload: result }) => {
+      if (!state.bestResult || state.bestResult > result) {
+        state.bestResult = result;
+      }
+    },
+  },
+});
+
+export const { updateBestResult } = scoreBoardSlice.actions;
+
+export const selectScoreBoardData = (state) => state.scoreBoardData;
+export const selectBestResult = (state) => state.scoreBoardData.bestResult;
+
+export default scoreBoardSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import gameReducer from "./features/gameSlice";
+import scoreBoardReducer from "./features/ScoreBoard/scoreBoardSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     gameData: gameReducer,
+    scoreBoardData: scoreBoardReducer,
   },
   middleware: [sagaMiddleware],
 });
