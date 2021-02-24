4a3842bcfb909d1809f4a8c8ce91a573b6d6713b
KalinaJakubowska <witillie@gmail.com>
2021-02-24 14:33:10 +0100

    move generateEmptyFields to saga, add watchPrepareGame

--- src/features/gameSaga.js
+++ src/features/gameSaga.js
@@ -9,7 +9,34 @@ import {
   setFirstID,
   generateFieldsContent,
   selectFirstID,
+  prepareGame,
+  setGameFields,
 } from "./gameSlice";
+
+const generateEmptyFields = (rows, columns) => {
+  let newGameFields = [];
+  const createNewField = ({ type, hidden }) => {
+    newGameFields.push({
+      id: newGameFields.length,
+      type,
+      hidden,
+      bombsAround: 0,
+      rightClicked: false,
+    });
+  };
+
+  for (let i = 0; i < rows; i++) {
+    for (let y = 0; y < columns; y++) {
+      if (y === 0 || y === columns - 1 || i === 0 || i === rows - 1) {
+        createNewField({ type: "border", hidden: false });
+      } else {
+        createNewField({ type: "field", hidden: true });
+      }
+    }
+  }
+
+  return newGameFields;
+};
 
 function* revealFieldHandler() {
   const { bombsNumber, gameFields } = yield select(selectGameData);
@@ -17,7 +44,7 @@ function* revealFieldHandler() {
   if (
     gameFields
       .filter(({ type }) => type === "bomb")
-      .find(({ hidden }) => hidden === false)
+      .find(({ hidden }) => !hidden)
   ) {
     yield put(setIsGameLost(true));
     yield put(revealAllBombs());
@@ -27,17 +54,29 @@ function* revealFieldHandler() {
   }
 }
 
-export function* watchRevealField() {
-  yield takeLatest(revealField.type, revealFieldHandler);
-  yield takeLatest(revealAllEmptyFieldsInGroup.type, revealFieldHandler);
-}
-
 function* startGame() {
   const firstID = yield select(selectFirstID);
   yield put(generateFieldsContent(firstID));
   yield put(revealAllEmptyFieldsInGroup({ id: firstID }));
 }
 
+function* prepareGameHandler(action) {
+  const rows = yield action.payload.rows;
+  const columns = yield action.payload.columns;
+
+  const newGameFields = yield generateEmptyFields(rows, columns);
+  yield put(setGameFields(newGameFields));
+}
+
+export function* watchRevealField() {
+  yield takeLatest(revealField.type, revealFieldHandler);
+  yield takeLatest(revealAllEmptyFieldsInGroup.type, revealFieldHandler);
+}
+
 export function* watchSetFirstID() {
   yield takeLatest(setFirstID.type, startGame);
+}
+
+export function* watchPrepareGame() {
+  yield takeLatest(prepareGame.type, prepareGameHandler);
 }
