26e3e387f7b56a676d5cb26161a4451fde3dff00
KalinaJakubowska <witillie@gmail.com>
2021-02-24 14:33:28 +0100

    add watchPrepareGame to saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from "redux-saga/effects";
-import { watchRevealField, watchSetFirstID } from "./features/gameSaga";
+import { watchRevealField, watchSetFirstID, watchPrepareGame } from "./features/gameSaga";
 
 export default function* rootSaga() {
-  yield all([watchRevealField(), watchSetFirstID()]);
+  yield all([watchRevealField(), watchSetFirstID(), watchPrepareGame()]);
 }
