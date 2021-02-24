fcab1683e4b76a1303b73973051926e407c32f23
KalinaJakubowska <witillie@gmail.com>
2021-02-24 14:28:43 +0100

    add bestResult displaying

--- src/features/ScoreBoard/index.js
+++ src/features/ScoreBoard/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectBestResult } from "./scoreBoardSlice";
 import { ScoreBoardWrapper } from "./styled";
 
 const ScoreBoard = () => {
-  return <ScoreBoardWrapper></ScoreBoardWrapper>;
+  const bestResult = useSelector(selectBestResult);
+
+  return <ScoreBoardWrapper>{bestResult}</ScoreBoardWrapper>;
 };
 
 export default ScoreBoard;
