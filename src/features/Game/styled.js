55fd091982a320c52c33e77c52ba02489593c3d1
KalinaJakubowska <witillie@gmail.com>
2021-03-29 23:47:07 +0200

    formatted

--- src/features/Game/styled.js
+++ src/features/Game/styled.js
@@ -2,46 +2,55 @@ import styled, { css } from "styled-components";
 import bomb from "./../../assets/icons/bomb.ico";
 
 export const GameBoard = styled.div`
-    width: fit-content;
-    margin: auto;
-    display: grid;
-    grid-template-columns: repeat(${({ columns }) => columns}, 40px);
-    grid-template-rows: repeat(${({ rows }) => rows}, 40px);
-`
+  width: fit-content;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(${({ columns }) => columns}, 40px);
+  grid-template-rows: repeat(${({ rows }) => rows}, 40px);
+`;
 export const GameField = styled.div`
-    background-color: ${({ theme }) => theme.color.second};
-    font-size: 35px;
-    text-align: center;
-    overflow: hidden;
-    user-select: none;
+  background-color: ${({ theme }) => theme.color.second};
+  font-size: 35px;
+  text-align: center;
+  overflow: hidden;
+  user-select: none;
 
-    ${({ type }) => type === "bomb" && css`
-        background-color: ${({ won, theme }) => won ? theme.color.winBomb : theme.color.loseBomb};
-        background-image: url(${bomb});
-        background-size: 25px;
-        background-repeat: no-repeat;
-        background-position: center;
+  ${({ type }) =>
+    type === "bomb" &&
+    css`
+      background-color: ${({ won, theme }) =>
+        won ? theme.color.winBomb : theme.color.loseBomb};
+      background-image: url(${bomb});
+      background-size: 25px;
+      background-repeat: no-repeat;
+      background-position: center;
     `}
 
-    ${({ type }) => type === "border" && css`
-        background-color: ${({ theme }) => theme.color.first};
+  ${({ type }) =>
+    type === "border" &&
+    css`
+      background-color: ${({ theme }) => theme.color.first};
     `}
 `;
 export const GameButton = styled.button`
-    display: block;
-    height: 40px;
-    width: 40px;
-    border-radius: 0;
-    background-color: ${({ theme }) => theme.color.shadow};
-    border: 2px outset ${({ theme }) => theme.color.second};
-    ${({ hidden }) => hidden && css`
-        display: none;
+  display: block;
+  height: 40px;
+  width: 40px;
+  border-radius: 0;
+  background-color: ${({ theme }) => theme.color.shadow};
+  border: 2px outset ${({ theme }) => theme.color.second};
+  ${({ hidden }) =>
+    hidden &&
+    css`
+      display: none;
     `}
 
-    ${({ rightClicked }) => rightClicked && css`
-    background-image: url(${bomb});
-    background-size: 25px;
-    background-repeat: no-repeat;
-    background-position: center;
+  ${({ rightClicked }) =>
+    rightClicked &&
+    css`
+      background-image: url(${bomb});
+      background-size: 25px;
+      background-repeat: no-repeat;
+      background-position: center;
     `}
 `;
